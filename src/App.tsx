1542f02c86997bc4d399ca51f378cc14bc5aa1a2
APPLE <apple@APPLEs-MacBook-Air.local>
2025-01-26 10:47:54 -0500

    Init to win it

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-lg flex items-center justify-center h-screen">
+       <p>INIT TO WIN IT</p>
+      </div>
+    </>
+  )
+}
+
+export default App
